ea653f5f980ee0f285f52360cd9407e5f5c2a2f8
Sushma-Ravindra <johndoe@example.com>
2024-05-10 21:23:23 +0530

    some changes

--- python/merge4images.py
+++ python/merge4images.py
@@ -9,7 +9,6 @@ image4 = cv2.imread('/Users/sushma/Desktop/filtered_image180.png')
 # Resize images to the same dimensions if necessary
 # You can use cv2.resize() for resizing
 
-
 # Superimpose the images
 result = cv2.addWeighted(image1, 0.5, image2, 0.5, 0)
 result = cv2.addWeighted(result, 0.5, image3, 0.5, 0)
